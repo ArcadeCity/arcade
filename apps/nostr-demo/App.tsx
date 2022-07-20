9614bfb6c99402ab6719ac6f36fc5e0a0eee6032
Christopher David <chris@arcade.city>
2022-07-20 15:14:42 -0500

    Create Expo 46 app and UI package with monorepo config (#2)
    * Root package.json
    
    * yarn.lock/gitignore
    
    * EXPO_BETA=1 yarn create expo-app (blank TS)
    
    * Add metro.config for monorepo
    
    * Change expo app entry file
    
    * Create ui package, test add to app
    
    * Add missing webpack config pkg for web
    
    * Add prettier config
    
    * Hello world
    
    * Format all via prettier

---
+++ apps/nostr-demo/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+// @ts-ignore
+import { greeting } from '@arcadecity/ui'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>{greeting}</Text>
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
