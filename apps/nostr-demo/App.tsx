11a46c348a38b6f9eba23812f59ac0bfc07cb0fe
Christopher David <chris@arcade.city>
2022-07-20 14:58:53 -0500

    Create ui package, test add to app

--- apps/nostr-demo/App.tsx
+++ apps/nostr-demo/App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+// @ts-ignore
+import { greeting } from '@arcadecity/ui'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{greeting}</Text>
       <StatusBar style="auto" />
     </View>
   );
