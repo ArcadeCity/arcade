c88c099820abe584e8ba5a60be84e32aa278aadc
Christopher David <chris@arcade.city>
2022-07-20 20:07:57 -0500

    Pull in nostr-tools locally; connect to nostr relay (web only)

--- apps/nostr-demo/nostr-tools/nip06.ts
+++ apps/nostr-demo/nostr-tools/nip06.ts
@@ -1,9 +1,3 @@
-/**
- * Doing our own version of https://github.com/fiatjaf/nostr-tools with native shims as necessary,
- * updated libraries (e.g. @scure/bip39 instead of micro-bip39), and better TypeScript support.
- * Otherwise will try to preserve the same API.
- */
-
 import { HDKey } from '@scure/bip32'
 import {
   generateMnemonic, mnemonicToSeedSync, validateMnemonic
--- apps/nostr-demo/nostr.ts
+++ apps/nostr-demo/nostr.ts
@@ -1,36 +1,33 @@
-import { getPublicKey, relayPool } from 'nostr-tools'
-// @ts-ignore
-import { generateSeedWords, privateKeyFromSeed, seedFromWords } from 'nostr-tools/nip06'
+import {
+  generateSeedWords, getPublicKey, privateKeyFromSeed, relayPool, seedFromWords
+} from './nostr-tools'
+
+let pubkey: string
 
 export const pool = relayPool()
-
-let pubkey: Uint8Array
 
 export const createNewAccount = () => {
   const mnemonic = generateSeedWords()
   const seed = seedFromWords(mnemonic)
   const priv = privateKeyFromSeed(seed)
-  pubkey = getPublicKey(priv)
+  pubkey = getPublicKey(Buffer.from(priv, 'hex'))
+  console.log({ mnemonic, seed, priv, pubkey })
   pool.setPrivateKey(priv)
   console.log(`Authed as ${pubkey}`)
-  pool.setPolicy('randomChoice', 3)
-  pool.addRelay('wss://relayer.fiatjaf.com')
-  pool.addRelay('wss://nostr-pub.wellorder.net')
-  pool.addRelay('wss://freedom-relay.herokuapp.com/ws')
+  pool.addRelay('wss://relay.damus.io')
 }
 
 export const subscribeToUser = async (pubkey: string) => {
   const onEvent = (event: any, relay: any) => {
     console.log(`Received event ${event.id ?? ''}`) // event, relay
   }
-
   pool.sub({
     cb: onEvent,
     filter: {
       author: pubkey,
     },
+    beforeSend: () => {},
   })
-
   console.log(`Subscribed to: ${pubkey}`)
   return true
 }
