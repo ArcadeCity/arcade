01dd767fa47c93ae976f3f0818e0a59346de65c3
Christopher David <chris@arcade.city>
2022-07-21 08:31:27 -0500

    normalize in try/catch

--- apps/nostr-demo/normalize.ts
+++ apps/nostr-demo/normalize.ts
@@ -1,22 +1,27 @@
 import { RideRequest } from './store'
 
 export const normalizeRideRequestEvent = (event: any) => {
-  const content = JSON.parse(event.content)
-  const rideRequest: RideRequest = {
-    amount: content.amount,
-    created_at: event.created_at,
-    expires: content.expires,
-    from: {
-      lat: content.from.lat,
-      lng: content.from.lng,
-    },
-    id: event.id,
-    name: content.name,
-    pubkey: event.pubkey,
-    sig: event.sig,
-    tags: event.tags,
-    type: content.type,
+  try {
+    const content = JSON.parse(event.content)
+    const rideRequest: RideRequest = {
+      amount: content.amount,
+      created_at: event.created_at,
+      expires: content.expires,
+      from: {
+        lat: content.from.lat,
+        lng: content.from.lng,
+      },
+      id: event.id,
+      name: content.name,
+      pubkey: event.pubkey,
+      sig: event.sig,
+      tags: event.tags,
+      type: content.type,
+    }
+    console.log(rideRequest)
+    return rideRequest
+  } catch (e) {
+    console.log('Could not normalize.', e)
+    return undefined
   }
-  console.log(rideRequest)
-  return rideRequest
 }
