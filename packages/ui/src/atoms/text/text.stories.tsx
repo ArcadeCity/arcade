74dee47b5124c1d747d918d128b8c6ef8d27f4db
Christopher David <chris@arcade.city>
2022-07-22 12:50:41 -0500

    Initial text story

---
+++ packages/ui/src/atoms/text/text.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta } from '@storybook/react'
+import { Text } from './text'
+
+export default {
+  component: Text,
+  title: 'Atoms/Text',
+} as Meta
+
+export const Basic: React.FC<{}> = () => <Text text='Hello world' />
+
+export const descriptionSlim: React.VFC<{}> = () => (
+  <Text text='Hello world' preset='descriptionSlim' />
+)
