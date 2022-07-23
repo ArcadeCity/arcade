d88dbb279c076aba530e5ec73818bf52ef846c3c
Christopher David <chris@arcade.city>
2022-07-22 23:29:46 -0500

    [arcade-city] Add theme (#9)
    * bump version, remove runtimeversion
    
    * TabBar icons and hide label
    
    * Axe useColorScheme / set background color
    
    * tabbar style
    
    * Header style
    
    * Try policy sdkversion
    
    * Use fonts
    
    * Header uses Lexend
    
    * tab active indicator
    
    * OTA updates working
    
    * Add Bugsnag and dependencies
    
    * Remove bugsnag - no web support
    
    * Delete bugsnag config

--- apps/arcade-city/navigation/index.tsx
+++ apps/arcade-city/navigation/index.tsx
@@ -1,29 +1,35 @@
 /**
  * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
  * https://reactnavigation.org/docs/getting-started
- *
  */
-import { FontAwesome } from '@expo/vector-icons'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import * as React from 'react'
-import { ColorSchemeName, Pressable } from 'react-native'
-
+import { ColorSchemeName, Pressable, View, ViewStyle } from 'react-native'
+import { color, palette, typography } from '@arcadecity/ui'
+import { FontAwesome, FontAwesome5 } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { DarkTheme, NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Colors from '../constants/Colors'
-import useColorScheme from '../hooks/useColorScheme'
 import ModalScreen from '../screens/ModalScreen'
 import NotFoundScreen from '../screens/NotFoundScreen'
 import TabOneScreen from '../screens/TabOneScreen'
 import TabTwoScreen from '../screens/TabTwoScreen'
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types'
+import {
+  RootStackParamList, RootTabParamList, RootTabScreenProps
+} from '../types'
 import LinkingConfiguration from './LinkingConfiguration'
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+const navTheme = {
+  colors: {
+    ...DarkTheme.colors,
+    background: color.background,
+  },
+  dark: true,
+}
+
+export default function Navigation() {
   return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <NavigationContainer linking={LinkingConfiguration} theme={navTheme}>
       <RootNavigator />
     </NavigationContainer>
   )
@@ -41,7 +47,19 @@ function RootNavigator() {
       <Stack.Screen name='Root' component={BottomTabNavigator} options={{ headerShown: false }} />
       <Stack.Screen name='NotFound' component={NotFoundScreen} options={{ title: 'Oops!' }} />
       <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name='Modal' component={ModalScreen} />
+        <Stack.Screen
+          name='Modal'
+          component={ModalScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: color.tabbar,
+            },
+            headerTitleStyle: {
+              color: color.text,
+              fontFamily: typography.secondary,
+            },
+          }}
+        />
       </Stack.Group>
     </Stack.Navigator>
   )
@@ -54,20 +72,37 @@ function RootNavigator() {
 const BottomTab = createBottomTabNavigator<RootTabParamList>()
 
 function BottomTabNavigator() {
-  const colorScheme = useColorScheme()
-
   return (
     <BottomTab.Navigator
       initialRouteName='TabOne'
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
+        tabBarActiveTintColor: palette.moonRaker,
+        tabBarInactiveTintColor: palette.blueBell,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: color.tabbar,
+          borderTopWidth: 2,
+          borderTopColor: palette.portGore,
+          paddingTop: 10,
+        },
       }}>
       <BottomTab.Screen
         name='TabOne'
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
           title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name='list-ul' color={color} focused={focused} />
+          ),
+          headerStyle: {
+            backgroundColor: color.tabbar,
+            borderBottomWidth: 2,
+            borderBottomColor: palette.portGore,
+          },
+          headerTitleStyle: {
+            color: color.text,
+            fontFamily: typography.secondary,
+          },
           headerRight: () => (
             <Pressable
               onPress={() => navigation.navigate('Modal')}
@@ -77,7 +112,7 @@ function BottomTabNavigator() {
               <FontAwesome
                 name='info-circle'
                 size={25}
-                color={Colors[colorScheme].text}
+                color={palette.moonRaker}
                 style={{ marginRight: 15 }}
               />
             </Pressable>
@@ -89,7 +124,18 @@ function BottomTabNavigator() {
         component={TabTwoScreen}
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name='user-alt' color={color} focused={focused} />
+          ),
+          headerStyle: {
+            backgroundColor: color.tabbar,
+            borderBottomWidth: 2,
+            borderBottomColor: palette.portGore,
+          },
+          headerTitleStyle: {
+            color: color.text,
+            fontFamily: typography.secondary,
+          },
         }}
       />
     </BottomTab.Navigator>
@@ -100,8 +146,30 @@ function BottomTabNavigator() {
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name']
   color: string
+  focused: boolean
+  name: React.ComponentProps<typeof FontAwesome5>['name']
 }) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />
+  return (
+    <>
+      <FontAwesome5 size={32} {...props} />
+      {props.focused && <View style={ACTIVE_INDICATOR} />}
+    </>
+  )
+}
+
+const ACTIVE_INDICATOR: ViewStyle = {
+  position: 'absolute',
+  bottom: -8,
+  height: 4,
+  width: 4,
+  borderRadius: 2,
+  backgroundColor: color.highlight,
+  shadowOffset: {
+    width: 0,
+    height: 0,
+  },
+  shadowOpacity: 1,
+  shadowRadius: 4,
+  shadowColor: 'rgb(244,89,244)', //color.highlight,
 }
--- apps/arcade-city/src/App.tsx
+++ apps/arcade-city/src/App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import useCachedResources from './hooks/useCachedResources'
-import useColorScheme from './hooks/useColorScheme'
-import Navigation from './navigation'
+import useCachedResources from '../hooks/useCachedResources'
+import Navigation from '../navigation'
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
-  const colorScheme = useColorScheme()
+  useEffect(() => {
+    // Alert.alert('Test1')
+  }, [])
   if (!isLoadingComplete) {
     return null
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <Navigation />
+        <StatusBar style='light' />
       </SafeAreaProvider>
     )
   }
