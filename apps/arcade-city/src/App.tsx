e347a547274b402188573ebea7058be1de41a20f
Christopher David <chris@arcade.city>
2022-07-23 08:40:59 -0500

    Axe alert

--- apps/arcade-city/src/App.tsx
+++ apps/arcade-city/src/App.tsx
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import useCachedResources from '../hooks/useCachedResources'
 import Navigation from '../navigation'
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
-  useEffect(() => {
-    // Alert.alert('Test1')
-  }, [])
   if (!isLoadingComplete) {
     return null
   } else {
