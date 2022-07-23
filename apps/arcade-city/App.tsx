b0eb6e8d3dec499aafd7340cfebd693e57449484
Christopher David <chris@arcade.city>
2022-07-22 23:07:45 -0500

    Add Bugsnag and dependencies

--- apps/arcade-city/App.tsx
+++ apps/arcade-city/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useEffect } from 'react'
-import { Alert } from 'react-native'
+import { Alert, View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import useCachedResources from './hooks/useCachedResources'
 import Navigation from './navigation'
@@ -8,7 +8,7 @@ import Navigation from './navigation'
 export default function App() {
   const isLoadingComplete = useCachedResources()
   useEffect(() => {
-    Alert.alert('we are updating...')
+    Alert.alert('Test1')
   }, [])
   if (!isLoadingComplete) {
     return null
--- apps/arcade-city/index.js
+++ apps/arcade-city/index.js
@@ -1,6 +1,11 @@
 import { registerRootComponent } from 'expo'
+import Bugsnag from '@bugsnag/expo'
 
 import App from './App'
+
+if (!__DEV__) {
+  Bugsnag.start()
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
