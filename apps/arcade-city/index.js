b7773e854f6489cd511f209e1c49ad333a4f5699
Christopher David <chris@arcade.city>
2022-07-23 12:05:33 -0500

    shh sentry in dev

--- apps/arcade-city/index.js
+++ apps/arcade-city/index.js
@@ -5,7 +5,7 @@ import App from './src/App'
 
 Sentry.init({
   dsn: 'https://b8e365e42bc742b3a34fecbacf81b5f5@o343125.ingest.sentry.io/6595759',
-  enableInExpoDevelopment: true,
+  enableInExpoDevelopment: false,
   debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 })
 
