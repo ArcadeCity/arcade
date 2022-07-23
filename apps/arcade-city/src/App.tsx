16d3807c03d40272277e4b4d8a78febdf8f274f9
Christopher David <chris@arcade.city>
2022-07-22 23:27:30 -0500

    Remove bugsnag - no web support

--- apps/arcade-city/src/App.tsx
+++ apps/arcade-city/src/App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useEffect } from 'react'
-import { Alert, View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import useCachedResources from './hooks/useCachedResources'
-import Navigation from './navigation'
+import useCachedResources from '../hooks/useCachedResources'
+import Navigation from '../navigation'
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
   useEffect(() => {
-    Alert.alert('Test1')
+    // Alert.alert('Test1')
   }, [])
   if (!isLoadingComplete) {
     return null
