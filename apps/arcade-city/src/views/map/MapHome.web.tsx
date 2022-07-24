fc5f7a928959af3d669435111a1ae16598aebf30
Christopher David <chris@arcade.city>
2022-07-24 18:32:40 -0500

    ArcadeMap on web too

--- apps/arcade-city/src/views/map/MapHome.web.tsx
+++ apps/arcade-city/src/views/map/MapHome.web.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import { StyleSheet, View } from 'react-native'
+import { ArcadeMap } from '@arcadecity/arcade-map'
 
 export const MapHome = () => {
-  return <View style={styles.container} />
+  return <ArcadeMap />
+  // return <View style={styles.container} />
 }
 
 const styles = StyleSheet.create({
