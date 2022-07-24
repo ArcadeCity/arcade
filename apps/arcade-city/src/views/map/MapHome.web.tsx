c2004176c601787f6620b9f61e50dd0fb19fcaf7
Christopher David <chris@arcade.city>
2022-07-24 17:59:33 -0500

    Retry with blank MapHome component for web

---
+++ apps/arcade-city/src/views/map/MapHome.web.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+export const MapHome = () => {
+  return <View style={styles.container} />
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
