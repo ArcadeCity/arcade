5c46bf26575efc52108b0e76f2d06eb039ffbfb3
Christopher David <chris@arcade.city>
2022-07-23 19:26:02 -0500

    fix up useCachedResources hook

--- apps/arcade-city/src/App.tsx
+++ apps/arcade-city/src/App.tsx
@@ -1,14 +1,12 @@
 import 'text-encoding-polyfill'
 import { StatusBar } from 'expo-status-bar'
+import { useCachedResources, useExpoUpdates } from 'lib/hooks'
 import React, { useMemo } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { LoadSplash } from 'views/loading'
-import useCachedResources from '../hooks/useCachedResources'
 import Navigation from '../navigation'
-import { useExpoUpdates } from './lib/hooks/useExpoUpdates'
 
 export default function App() {
-  // const isLoadingComplete = false
   const isLoadingComplete = useCachedResources()
   useExpoUpdates(3)
 
