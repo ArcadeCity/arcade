ac9dab60dfa542c50482d639a98f37a9e3b7739c
Christopher David <chris@arcade.city>
2022-07-25 08:29:03 -0500

    Initial ArcadeMap

--- apps/arcade-city/src/views/map/ArcadeMap.tsx
+++ apps/arcade-city/src/views/map/ArcadeMap.tsx
@@ -1,8 +1,9 @@
-import { Canvas, RootState } from '@react-three/fiber/native'
 import { ExpoWebGLRenderingContext } from 'expo-gl'
-import { DeviceOrientationControls, MetaMap, Stars, Tile } from 'lib/arcangel'
+// import { DeviceOrientationControls, MetaMap, Stars, Tile } from 'lib/arcangel'
 import { useState } from 'react'
-import { HUD } from './HUD'
+import { Canvas, RootState } from '@react-three/fiber/native'
+
+// import { HUD } from './HUD'
 
 export const ArcadeMap = () => {
   const [cv, setCv] = useState<HTMLCanvasElement | null>()
@@ -16,18 +17,15 @@ export const ArcadeMap = () => {
   }
 
   return (
-    <>
-      <Canvas onCreated={onCreated} camera={{ position: [0, 10, 0] }}>
-        <color attach='background' args={[0x1c133a]} />
-        <fog attach='fog' color='#1c133a' near={10} far={400} />
-        <pointLight position={[10, 10, 10]} />
-        <ambientLight intensity={0.8} />
-        <Tile position={[0, 1, -16]} rotation={[Math.PI / 2, 0, 0]} />
-        <MetaMap canvas={cv} context={context} />
-        <Stars />
-        <DeviceOrientationControls />
-      </Canvas>
-      <HUD />
-    </>
+    <Canvas onCreated={onCreated} camera={{ position: [0, 10, 0] }}>
+      <color attach='background' args={[0x1c133a]} />
+      <fog attach='fog' color='#1c133a' near={10} far={400} />
+      <pointLight position={[10, 10, 10]} />
+      <ambientLight intensity={0.8} />
+      {/* <Tile position={[0, 1, -16]} rotation={[Math.PI / 2, 0, 0]} /> */}
+      {/* <MetaMap canvas={cv} context={context} /> */}
+      {/* <Stars /> */}
+      {/* <DeviceOrientationControls /> */}
+    </Canvas>
   )
 }
