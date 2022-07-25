91177d2aaba56674030e904c38fb579a57049e1f
Christopher David <chris@arcade.city>
2022-07-25 09:12:49 -0500

    Pull in harp-gen map-demo

---
+++ apps/map-demo/index.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2019-2021 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { GeoCoordinates } from "@here/harp-geoutils";
+
+import { View } from "./View";
+
+const app = new View({
+    canvas: document.getElementById("map") as HTMLCanvasElement
+});
+
+const mapView = app.mapView;
+
+// make map full-screen
+mapView.resize(window.innerWidth, window.innerHeight);
+
+// react on resize events from the browser.
+window.addEventListener("resize", () => {
+    mapView.resize(window.innerWidth, window.innerHeight);
+});
+
+// center the camera to New York
+mapView.lookAt({ target: new GeoCoordinates(40.70398928, -74.01319808), zoomLevel: 17, tilt: 40 });
+
+// make sure the map is rendered
+mapView.update();
