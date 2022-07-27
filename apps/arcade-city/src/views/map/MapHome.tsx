6f2f11493558d0d6e85762ccc663c370028bbe31
Christopher David <chris@arcade.city>
2022-07-26 19:56:58 -0500

    Swap in diff map

--- apps/arcade-city/src/views/map/MapHome.tsx
+++ apps/arcade-city/src/views/map/MapHome.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 import { WebView } from 'react-native-webview'
 
 export const MapHome = () => {
-  return <WebView style={styles.container} source={{ uri: 'https://blitzmap.vercel.app' }} />
+  return <WebView style={styles.container} source={{ uri: 'https://map.arcade.city' }} />
 }
 
 const styles = StyleSheet.create({
