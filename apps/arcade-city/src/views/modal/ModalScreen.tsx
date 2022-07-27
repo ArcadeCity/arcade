7642031ccea4f708b26446e611189f03768bbd75
Christopher David <chris@arcade.city>
2022-07-27 16:53:05 -0500

    Add text to feed screen

--- apps/arcade-city/src/views/modal/ModalScreen.tsx
+++ apps/arcade-city/src/views/modal/ModalScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Text } from '@arcadecity/ui'
+import { spacing, Text } from '@arcadecity/ui'
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text preset='description' text="Isn't this cool?!" />
+      <Text
+        preset='description'
+        text='Please try out the ride request flow and check that the request shows up in the feed.'
+      />
+      <Text
+        preset='description'
+        text='Your requests are sent to the public Nostr network and will be visible to all Nostr clients.'
+      />
     </View>
   )
 }
@@ -13,7 +20,8 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: spacing[4],
   },
 })
