a97b23c87632cb5d9caed01eba177cf2c5c4d51d
Christopher David <chris@arcade.city>
2022-07-27 13:39:22 -0500

    Add missing RequestConfirm icons

--- apps/arcade-city/src/views/theme/images.ts
+++ apps/arcade-city/src/views/theme/images.ts
@@ -1,5 +1,10 @@
 export const images = {
   close: require('../../../assets/images/close.png'),
+  dollar: require('../../../assets/images/dollar.png'),
+  guilds: require('../../../assets/images/guild.png'),
+  guildsActive: require('../../../assets/images/guild-active.png'),
+  inbox: require('../../../assets/images/inbox.png'),
+  inboxActive: require('../../../assets/images/inbox-active.png'),
   map: require('../../../assets/images/map.png'),
   mapActive: require('../../../assets/images/map-active.png'),
   rider: require('../../../assets/images/riderguy.png'),
