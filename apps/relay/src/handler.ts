bc1b2292d569da98959ab989d7a48358881d9a2b
Christopher David <chris@arcade.city>
2022-07-28 18:17:55 -0500

    Add error handler for message parsing

--- apps/relay/src/handler.ts
+++ apps/relay/src/handler.ts
@@ -3,16 +3,21 @@ import { formatNotice } from '@arcadecity/nostr-utils'
 import { handleRequest } from './nip01'
 
 export const handler = (ws: WebSocket, message: string) => {
-  console.log(`[Received message]: ${message}`)
-  const parsedMessage = JSON.parse(message)
-
-  switch (parsedMessage[0]) {
-    case 'REQ':
-      const subId = parsedMessage[1]
-      const filters = parsedMessage[2]
-      handleRequest(subId, filters)
-      break
-    default:
-      ws.send(formatNotice('Unknown message type'))
+  try {
+    const parsedMessage = JSON.parse(message)
+    console.log(`Parsed message:`, parsedMessage)
+    switch (parsedMessage[0]) {
+      case 'REQ':
+        const subId = parsedMessage[1]
+        const filters = parsedMessage[2]
+        handleRequest(subId, filters)
+        break
+      default:
+        ws.send(formatNotice('Unknown message type'))
+    }
+  } catch (e) {
+    const msg = 'Failed to parse message'
+    console.log(`${msg}:`, message)
+    ws.send(formatNotice(msg))
   }
 }
