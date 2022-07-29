8e2b2808a6903a1abadd8659979541528f9dd885
Christopher David <chris@arcade.city>
2022-07-28 21:11:56 -0500

    yarn add -E edgedb@latest

--- packages/ui/dist/index.js
+++ packages/ui/dist/index.js
@@ -1,81 +1,71 @@
-'use strict'
-var __create = Object.create
-var __defProp = Object.defineProperty
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor
-var __getOwnPropNames = Object.getOwnPropertyNames
-var __getProtoOf = Object.getPrototypeOf
-var __hasOwnProp = Object.prototype.hasOwnProperty
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __export = (target, all) => {
-  for (var name in all) __defProp(target, name, { get: all[name], enumerable: true })
-}
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
 var __copyProps = (to, from, except, desc) => {
-  if ((from && typeof from === 'object') || typeof from === 'function') {
+  if (from && typeof from === "object" || typeof from === "function") {
     for (let key of __getOwnPropNames(from))
       if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, {
-          get: () => from[key],
-          enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable,
-        })
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  return to
-}
-var __toESM = (mod, isNodeMode, target) => (
-  (target = mod != null ? __create(__getProtoOf(mod)) : {}),
-  __copyProps(
-    isNodeMode || !mod || !mod.__esModule
-      ? __defProp(target, 'default', { value: mod, enumerable: true })
-      : target,
-    mod
-  )
-)
-var __toCommonJS = (mod) => __copyProps(__defProp({}, '__esModule', { value: true }), mod)
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target, mod));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // src/index.tsx
-var src_exports = {}
+var src_exports = {};
 __export(src_exports, {
   ACTIVE_OPACITY: () => ACTIVE_OPACITY,
   Text: () => Text,
   color: () => color,
   palette: () => palette,
   spacing: () => spacing,
-  typography: () => typography,
-})
-module.exports = __toCommonJS(src_exports)
+  typography: () => typography
+});
+module.exports = __toCommonJS(src_exports);
 
 // src/atoms/text/text.tsx
-var import_ramda = require('ramda')
-var React = __toESM(require('react'))
-var import_react_native2 = require('react-native')
+var import_ramda = require("ramda");
+var React = __toESM(require("react"));
+var import_react_native2 = require("react-native");
 
 // src/theme/palette.ts
 var palette = {
-  arwes: 'rgb(0, 248, 248)',
-  arwesTitle: '#a1ecfb',
-  arwesText: '#26dafd',
-  arwesFade: 'rgba(0, 248, 248, 0.5)',
-  arwesFader: 'rgba(0, 248, 248, 0.3)',
-  arwesSecondary: 'rgb(6,61,62)',
-  white: '#FFFFFF',
-  black: '#000000',
-  haiti: '#120B29',
-  purple: '#1C133A',
-  portGore: '#2D2252',
-  blueBell: '#9D98CB',
-  blueBellFaded: 'rgba(157, 152, 203, 0.6)',
-  minsk: '#46367C',
-  moonRaker: '#EEECFB',
-  radicalRed: '#FC3A57',
-  pinkFlamingo: '#F459F4',
-  electricViolet: '#AE30FF',
-  electricIndigo: '#5B20F2',
-  blueBright: '#66B3F8',
-}
+  arwes: "rgb(0, 248, 248)",
+  arwesTitle: "#a1ecfb",
+  arwesText: "#26dafd",
+  arwesFade: "rgba(0, 248, 248, 0.5)",
+  arwesFader: "rgba(0, 248, 248, 0.3)",
+  arwesSecondary: "rgb(6,61,62)",
+  white: "#FFFFFF",
+  black: "#000000",
+  haiti: "#120B29",
+  purple: "#1C133A",
+  portGore: "#2D2252",
+  blueBell: "#9D98CB",
+  blueBellFaded: "rgba(157, 152, 203, 0.6)",
+  minsk: "#46367C",
+  moonRaker: "#EEECFB",
+  radicalRed: "#FC3A57",
+  pinkFlamingo: "#F459F4",
+  electricViolet: "#AE30FF",
+  electricIndigo: "#5B20F2",
+  blueBright: "#66B3F8"
+};
 
 // src/theme/color.ts
-var ACTIVE_OPACITY = 0.8
+var ACTIVE_OPACITY = 0.8;
 var color = {
   palette,
-  transparent: 'rgba(0, 0, 0, 0)',
+  transparent: "rgba(0, 0, 0, 0)",
   background: palette.haiti,
   primary: palette.electricIndigo,
   secondary: palette.moonRaker,
@@ -92,48 +82,40 @@ var color = {
   destination: palette.pinkFlamingo,
   highlight: palette.pinkFlamingo,
   error: palette.radicalRed,
-  shadow: palette.haiti,
-}
+  shadow: palette.haiti
+};
 
 // src/theme/spacing.ts
-var spacing = [0, 4, 8, 12, 16, 24, 32, 48, 64]
+var spacing = [0, 4, 8, 12, 16, 24, 32, 48, 64];
 
 // src/theme/typography.ts
-var import_react_native = require('react-native')
+var import_react_native = require("react-native");
 var typography = {
   primary: import_react_native.Platform.select({
-    ios: 'Inter_400Regular',
-    android: 'Inter_400Regular',
-    web: 'Inter_400Regular',
+    ios: "Inter_400Regular",
+    android: "Inter_400Regular",
+    web: "Inter_400Regular"
   }),
-  bold: import_react_native.Platform.select({
-    ios: 'Inter_700Bold',
-    android: 'Inter_700Bold',
-    web: 'Inter_700Bold',
-  }),
+  bold: import_react_native.Platform.select({ ios: "Inter_700Bold", android: "Inter_700Bold", web: "Inter_700Bold" }),
   secondary: import_react_native.Platform.select({
-    ios: 'Lexend_700Bold',
-    android: 'Lexend_700Bold',
-    web: 'Lexend_700Bold',
+    ios: "Lexend_700Bold",
+    android: "Lexend_700Bold",
+    web: "Lexend_700Bold"
   }),
-  code: import_react_native.Platform.select({
-    ios: 'Courier',
-    android: 'monospace',
-    web: 'monospace',
-  }),
-}
+  code: import_react_native.Platform.select({ ios: "Courier", android: "monospace", web: "monospace" })
+};
 
 // src/atoms/text/text.presets.ts
 var BASE = {
   fontFamily: typography.primary,
   color: color.palette.moonRaker,
   fontSize: 15,
-  lineHeight: 22,
-}
+  lineHeight: 22
+};
 var SECONDARY = {
   ...BASE,
-  color: color.palette.blueBell,
-}
+  color: color.palette.blueBell
+};
 var presets = {
   default: BASE,
   bold: { ...BASE, fontFamily: typography.bold },
@@ -144,20 +126,20 @@ var presets = {
     fontSize: 28,
     lineHeight: 34,
     marginVertical: spacing[2],
-    fontFamily: typography.secondary,
+    fontFamily: typography.secondary
   },
   title2: {
     ...BASE,
     fontSize: 22,
     lineHeight: 28,
-    marginVertical: spacing[2],
+    marginVertical: spacing[2]
   },
   title3: {
     ...BASE,
     fontSize: 24,
     lineHeight: 28,
     marginVertical: spacing[2],
-    fontFamily: typography.bold,
+    fontFamily: typography.bold
   },
   description: { ...SECONDARY, marginBottom: spacing[5] - 2 },
   descriptionSlim: { ...SECONDARY },
@@ -166,18 +148,18 @@ var presets = {
     ...BASE,
     lineHeight: 16,
     fontFamily: typography.bold,
-    color: color.error,
+    color: color.error
   },
   labelAccept: {
     ...BASE,
     lineHeight: 16,
     fontFamily: typography.bold,
-    color: color.primary,
+    color: color.primary
   },
   secondaryLabel: {
     color: color.secondaryText,
     fontSize: 16,
-    lineHeight: 30,
+    lineHeight: 30
   },
   sectionHeader: {
     ...SECONDARY,
@@ -185,38 +167,38 @@ var presets = {
     lineHeight: 14,
     fontFamily: typography.bold,
     letterSpacing: 1,
-    marginBottom: spacing[2],
+    marginBottom: spacing[2]
   },
   error: {
     ...BASE,
     fontSize: 12,
     lineHeight: 14,
     color: color.error,
-    marginTop: spacing[2],
+    marginTop: spacing[2]
   },
   link: {
     ...BASE,
     color: color.link,
-    fontFamily: typography.bold,
+    fontFamily: typography.bold
   },
   small: {
     ...SECONDARY,
     fontSize: 11,
     lineHeight: 14,
-    color: color.palette.moonRaker,
+    color: color.palette.moonRaker
   },
   detail: {
     ...BASE,
     fontSize: 9,
     lineHeight: 11,
-    fontFamily: typography.bold,
-  },
-}
+    fontFamily: typography.bold
+  }
+};
 
 // src/atoms/text/text.tsx
 var Text = (props) => {
   const {
-    preset = 'default',
+    preset = "default",
     capitalize: capitalized = false,
     tx,
     txOptions,
@@ -224,32 +206,27 @@ var Text = (props) => {
     children,
     style: styleOverride,
     ...rest
-  } = props
-  const whichText = text || ''
-  const newText = capitalized ? capitalize(whichText) : whichText
-  const content = newText || children
-  const style = presets[preset] || presets.default
-  const styles = (0, import_ramda.flatten)([style, styleOverride])
-  return /* @__PURE__ */ React.createElement(
-    import_react_native2.Text,
-    {
-      key: tx,
-      ...rest,
-      style: styles,
-    },
-    content
-  )
-}
+  } = props;
+  const whichText = text || "";
+  const newText = capitalized ? capitalize(whichText) : whichText;
+  const content = newText || children;
+  const style = presets[preset] || presets.default;
+  const styles = (0, import_ramda.flatten)([style, styleOverride]);
+  return /* @__PURE__ */ React.createElement(import_react_native2.Text, {
+    key: tx,
+    ...rest,
+    style: styles
+  }, content);
+};
 function capitalize(theString) {
-  return theString.charAt(0).toUpperCase() + theString.slice(1)
+  return theString.charAt(0).toUpperCase() + theString.slice(1);
 }
 // Annotate the CommonJS export names for ESM import in node:
-0 &&
-  (module.exports = {
-    ACTIVE_OPACITY,
-    Text,
-    color,
-    palette,
-    spacing,
-    typography,
-  })
+0 && (module.exports = {
+  ACTIVE_OPACITY,
+  Text,
+  color,
+  palette,
+  spacing,
+  typography
+});
