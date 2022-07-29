6229ec08d30229965cf01c6a6bce600d20e3eb2b
Christopher David <chris@arcade.city>
2022-07-29 15:56:05 -0500

    that is some sexy query/limit syntax

--- apps/relay/src/nip01/handleRequest.ts
+++ apps/relay/src/nip01/handleRequest.ts
@@ -1,4 +1,8 @@
+import * as edgedb from 'edgedb'
 import { Filters } from '@arcadecity/nostr-utils'
+import e from '../../dbschema/edgeql-js'
+
+const client = edgedb.createClient()
 
 /**
  * Upon receiving a REQ message, the relay SHOULD query its internal database and return events that
@@ -6,10 +10,19 @@ import { Filters } from '@arcadecity/nostr-utils'
  * websocket until the websocket is closed.
  */
 
-export const handleRequest = (subId: string, filters: Filters) => {
+export const handleRequest = async (subId: string, filters: Filters) => {
   console.log(`Handling request for subId ${subId} and filters:`, filters)
   if (!subId) {
     console.error('No subId provided')
     return
   }
+
+  const query = e.select(e.Event, () => ({
+    ...e.Event['*'],
+    limit: filters.limit,
+  }))
+
+  const result = await query.run(client)
+  console.log('result:', result)
+  console.log('result.length:', result.length)
 }
