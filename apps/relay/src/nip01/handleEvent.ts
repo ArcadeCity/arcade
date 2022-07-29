def0d7b889df8af88d7c01a9a3e64ceec3f91b94
Christopher David <chris@arcade.city>
2022-07-28 21:15:12 -0500

    console log client

--- apps/relay/src/nip01/handleEvent.ts
+++ apps/relay/src/nip01/handleEvent.ts
@@ -1,5 +1,9 @@
+import * as edgedb from 'edgedb'
 import { NostrEvent } from '@arcadecity/nostr-utils'
 
+const client = edgedb.createClient()
+
 export const handleEvent = (event: NostrEvent) => {
+  console.log('Handling event, got edgedb client:', client)
   // console.log('Handling event:', event)
 }
