8403be62e3c0a98236f3007fb3ac77c29921fc04
Christopher David <chris@arcade.city>
2022-07-29 11:45:29 -0500

    Delete old demo apps (#25)
    * Delete bullrun-demo
    
    * Delete map-demo
    
    * Delete next app
    
    * yarn.lock

--- apps/bullrun-demo/nostr-tools/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Doing our own version of https://github.com/fiatjaf/nostr-tools with native shims as necessary,
- * updated libraries (e.g. @scure/bip39 instead of micro-bip39), and better TypeScript support.
- * Otherwise will try to preserve the same API.
- */
-
-export * from './keys'
-export * from './nip06'
-export * from './pool'
