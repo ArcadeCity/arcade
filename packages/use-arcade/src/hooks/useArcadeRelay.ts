49d256eca09d07e790e7966c66aae912406d0afc
Christopher David <chris@arcade.city>
2022-08-01 10:04:23 -0500

    Initial useArcadeRelay hook

---
+++ packages/use-arcade/src/hooks/useArcadeRelay.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useArcadeRelay = () => {
+  const [isPaused, setPause] = useState(false)
+  const ws = useRef<WebSocket | null>(null)
+
+  useEffect(() => {
+    ws.current = new WebSocket('wss://relay.arcade.city')
+    ws.current.onopen = () => console.log('ws opened')
+    ws.current.onclose = () => console.log('ws closed')
+
+    const wsCurrent = ws.current
+
+    return () => {
+      wsCurrent.close()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!ws.current) return
+
+    ws.current.onmessage = (e) => {
+      if (isPaused) return
+      const message = JSON.parse(e.data)
+      console.log('e', message)
+    }
+  }, [isPaused])
+
+  return [isPaused, setPause]
+}
