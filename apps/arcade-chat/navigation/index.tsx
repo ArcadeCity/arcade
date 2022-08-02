306846a6fc3b5f6f47d97b573ab0300d0dfe3808
Christopher David <chris@arcade.city>
2022-08-02 16:50:04 -0600

    Fix UI depending on RN 0.69.1

--- apps/arcade-chat/navigation/index.tsx
+++ apps/arcade-chat/navigation/index.tsx
@@ -9,6 +9,7 @@ import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import * as React from 'react'
 import { ColorSchemeName, Pressable } from 'react-native'
+import { color, palette } from '@arcadecity/ui'
 
 import Colors from '../constants/Colors'
 import useColorScheme from '../hooks/useColorScheme'
