96e22b9b1204391564e95e7c8df8cd8d69e39afd
Christopher David <chris@arcade.city>
2022-08-02 16:18:32 -0600

    ChannelList

--- apps/arcade-chat/components/ChannelList.tsx
+++ apps/arcade-chat/components/ChannelList.tsx
@@ -1,11 +1,26 @@
-import { Channel, NostrEvent, useChannelsCreated } from '@arcadecity/use-arcade'
-import { FlatList, Text } from 'react-native'
+import { Channel, useChannelsCreated } from '@arcadecity/use-arcade'
+import { FlatList, StyleSheet, Text } from 'react-native'
 
 export const ChannelList = () => {
   const channels: Channel[] = useChannelsCreated()
-  return <FlatList data={channels} keyExtractor={keyExtractor} renderItem={renderItem} />
+  return (
+    <FlatList
+      data={channels}
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+      style={[styles.flatList, { backgroundColor: '#120B29' }]}
+    />
+  )
 }
 
 const keyExtractor = (item: Channel) => item.id
 
-const renderItem = ({ item }: { item: Channel }) => <Text>{item.name}</Text>
+const renderItem = ({ item }: { item: Channel }) => (
+  <Text style={{ color: '#EEECFB' }}>{item.name}</Text>
+)
+
+const styles = StyleSheet.create({
+  flatList: { flex: 1 },
+  flatListContentContainer: { flexGrow: 1 },
+  statusIndicator: { left: 0, position: 'absolute', right: 0, top: 0 },
+})
--- apps/arcade-chat/screens/TabOneScreen.tsx
+++ apps/arcade-chat/screens/TabOneScreen.tsx
@@ -1,20 +1,12 @@
-import { Button, StyleSheet } from 'react-native'
-import { useChannelsCreated } from '@arcadecity/use-arcade'
-import { Text, View } from '../components/Themed'
+import { StyleSheet } from 'react-native'
+import { View } from '../components/Themed'
 import { RootTabScreenProps } from '../types'
 import { ChannelList } from '../components/ChannelList'
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  // const channels = useChannelsCreated()
-  // useArcadeRelay()
-  // const createChannel = () => {
-  //   console.log('create channel')
-  // }
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.title}>{channels.length} channels</Text> */}
       <ChannelList />
-      {/* <Button onPress={createChannel} title='Create Channel' /> */}
     </View>
   )
 }
@@ -22,8 +14,6 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
