4e25464762517232d8abdb1ecf6bfa4f7625fe91
Christopher David <chris@arcade.city>
2022-08-01 20:10:56 -0600

    useArcadeRelay works

--- apps/nostr-client/App.tsx
+++ apps/nostr-client/App.tsx
@@ -1,22 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { useArcadeRelay } from '@arcadecity/use-arcade'
+import useCachedResources from './hooks/useCachedResources'
+import useColorScheme from './hooks/useColorScheme'
+import Navigation from './navigation'
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+export const App = () => {
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
+  useArcadeRelay()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
-    );
+    )
   }
 }
