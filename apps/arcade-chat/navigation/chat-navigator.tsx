b8e9f9095bc72a5c0f62cc87c3b3c210a9c9394e
Christopher David <chris@arcade.city>
2022-08-02 18:58:57 -0600

    Navbutton back for Channel

--- apps/arcade-chat/navigation/chat-navigator.tsx
+++ apps/arcade-chat/navigation/chat-navigator.tsx
@@ -3,21 +3,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import TabOneScreen from '../screens/TabOneScreen'
 import { ChannelScreen } from '../screens/ChannelScreen'
 import { stackOptions } from './stackOptions'
+import { useNavigation } from '@react-navigation/native'
+import { NavButton } from '../components/nav-button'
 
 const Stack = createNativeStackNavigator()
 
-export const ChatNavigator = () => (
-  <Stack.Navigator initialRouteName='chathome'>
-    <Stack.Screen
-      name='chathome'
-      component={TabOneScreen}
-      options={{ ...stackOptions, title: 'Channels' }}
-    />
-    <Stack.Screen
-      name='channel'
-      component={ChannelScreen}
-      options={{ ...stackOptions, title: 'Channel' }}
-    />
-    {/* <Stack.Screen name='profile' component={Profile} options={stackOptions} /> */}
-  </Stack.Navigator>
-)
+export const ChatNavigator = () => {
+  const navigation = useNavigation()
+  return (
+    <Stack.Navigator initialRouteName='chathome'>
+      <Stack.Screen
+        name='chathome'
+        component={TabOneScreen}
+        options={{ ...stackOptions, title: 'Channels' }}
+      />
+      <Stack.Screen
+        name='channel'
+        component={ChannelScreen}
+        options={{
+          ...stackOptions,
+          title: 'Channel',
+          headerLeft: () => <NavButton onPress={navigation.goBack} />,
+        }}
+      />
+      {/* <Stack.Screen name='profile' component={Profile} options={stackOptions} /> */}
+    </Stack.Navigator>
+  )
+}
