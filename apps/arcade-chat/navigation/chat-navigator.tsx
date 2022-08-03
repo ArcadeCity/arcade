bca81b45d9c4f7599e5b2d2fdd299ffe099e6e46
Christopher David <chris@arcade.city>
2022-08-03 07:59:41 -0600

    initial updateDemoChannelMetadata

--- apps/arcade-chat/navigation/chat-navigator.tsx
+++ apps/arcade-chat/navigation/chat-navigator.tsx
@@ -5,11 +5,17 @@ import { ChannelScreen } from '../screens/ChannelScreen'
 import { stackOptions } from './stackOptions'
 import { useNavigation } from '@react-navigation/native'
 import { NavButton } from '../components/nav-button'
+import { Pressable } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+import { palette } from '@arcadecity/ui'
 
 const Stack = createNativeStackNavigator()
 
 export const ChatNavigator = () => {
   const navigation = useNavigation()
+  const demoUpdateMetadata = () => {
+    console.log('Lets try this')
+  }
   return (
     <Stack.Navigator initialRouteName='chathome'>
       <Stack.Screen
@@ -24,6 +30,21 @@ export const ChatNavigator = () => {
           ...stackOptions,
           title: 'Channel',
           headerLeft: () => <NavButton onPress={navigation.goBack} />,
+          headerRight: () => (
+            <Pressable
+              onPress={demoUpdateMetadata}
+              // onPress={() => navigation.navigate('Modal')}
+              style={({ pressed }) => ({
+                opacity: pressed ? 0.5 : 1,
+              })}>
+              <FontAwesome
+                name='info-circle'
+                size={25}
+                color={palette.moonRaker}
+                style={{ marginRight: 15 }}
+              />
+            </Pressable>
+          ),
         }}
       />
       {/* <Stack.Screen name='profile' component={Profile} options={stackOptions} /> */}
---
+++ packages/use-arcade/src/demo/updateDemoChannelMetadata.ts
@@ -0,0 +1,44 @@
+import {
+  createNewAccount,
+  getEventHash,
+  NostrEvent,
+  NostrEventToSerialize,
+  NostrEventToSign,
+  NostrKind,
+  signEvent,
+} from '../nostr'
+import { ChannelMetadata } from '../store'
+
+export const updateDemoChannelMetadata = async (channelId: string) => {
+  const { pubkey, privkey } = createNewAccount()
+  const date = new Date()
+  const dateTimeInSeconds = Math.floor(date.getTime() / 1000)
+  const twohundy = Math.round(200 + Math.random() * 5)
+  const photo = `https://placekitten.com/200/${twohundy.toString()}`
+  const channelMetadata = {
+    about: `test about metadadtatatata ${twohundy}`,
+    channelId,
+    name: `test name ${twohundy}`,
+    picture: photo,
+    type: 'public', // maybe 'public', 'private', 'geohash', 'geocoords'
+  }
+  const nostrEventToSerialize: NostrEventToSerialize = {
+    created_at: dateTimeInSeconds,
+    kind: NostrKind.channelmetadata,
+    tags: [],
+    content: JSON.stringify(channelMetadata),
+    pubkey,
+  }
+  const id = getEventHash(nostrEventToSerialize)
+  const nostrEventToSign: NostrEventToSign = {
+    ...nostrEventToSerialize,
+    id,
+  }
+  const sig = await signEvent(nostrEventToSign, privkey)
+  const nostrEvent: NostrEvent = {
+    ...nostrEventToSerialize,
+    id,
+    sig,
+  } as ChannelMetadata
+  return nostrEvent
+}
