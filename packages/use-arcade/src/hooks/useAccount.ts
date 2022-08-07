0eb29b81f9d0938166d5c47bb54d0b0b781056cd
Christopher David <chris@arcade.city>
2022-08-06 20:58:20 -0500

    Save/retrieve keys from secure device storage

--- packages/use-arcade/src/hooks/useAccount.ts
+++ packages/use-arcade/src/hooks/useAccount.ts
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import { createNewAccount } from '../nostr'
-import { Account, store } from '../store'
+import { Account, AccountKeys, store } from '../store'
 import { useAccountKeys } from './useAccountKeys'
 import { useAccountMetadata } from './useAccountMetadata'
+import * as SecureStore from 'expo-secure-store'
 
 export const useAccount: () => Account | null = () => {
   const [loading, setLoading] = useState(true)
@@ -13,17 +14,42 @@ export const useAccount: () => Account | null = () => {
     metadata,
   }
 
+  const checkForKeys = async () => {
+    const storeAvailable = await SecureStore.isAvailableAsync()
+    if (storeAvailable) {
+      const publicKey = await SecureStore.getItemAsync('ARCADE_NPUB')
+      const privateKey = await SecureStore.getItemAsync('ARCADE_NSEC')
+      const mnemonic = await SecureStore.getItemAsync('ARCADE_MNEMONIC')
+
+      if (publicKey && privateKey && mnemonic) {
+        const keys: AccountKeys = { publicKey, privateKey, mnemonic }
+        store.accountKeys = keys
+        console.log('Set account keys!')
+        return
+      }
+    }
+
+    console.log('Creating new keys.')
+    const newAccountKeys = createNewAccount()
+    store.accountKeys = newAccountKeys
+    store.accountMetadata = {
+      name: 'ArcadeAnon',
+      about: 'New user',
+      picture: 'https://placekitten.com/200/200',
+    }
+    if (storeAvailable) {
+      await SecureStore.setItemAsync('ARCADE_NPUB', newAccountKeys.publicKey)
+      await SecureStore.setItemAsync('ARCADE_NSEC', newAccountKeys.privateKey)
+      await SecureStore.setItemAsync('ARCADE_MNEMONIC', newAccountKeys.mnemonic)
+    }
+    setLoading(false)
+  }
+
   useEffect(() => {
     if (!keys || !keys.privateKey || !keys.publicKey || !keys.mnemonic) {
-      console.log('Creating new keys.')
-      const newAccountKeys = createNewAccount()
-      store.accountKeys = newAccountKeys
-      store.accountMetadata = {
-        name: 'ArcadeAnon',
-        about: 'New user',
-        picture: 'https://placekitten.com/200/200',
-      }
-      setLoading(false)
+      // So the store has no keys. Maybe we tried rehydrating (unimpl.)
+      // Now check if we have keys in device storage.
+      checkForKeys()
     } else if (keys) {
       console.log('We got keys! Pub:', keys.publicKey)
       return
