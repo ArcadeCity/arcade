56444d406b0aa655484ab913d9abd9b1ac5b72bf
Christopher David <chris@arcade.city>
2022-08-09 15:07:35 -0500

    show channels on screen

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,13 +1,19 @@
 import { useSx, View, H1, P, Row, A } from 'dripsy'
 import { TextLink } from 'solito/link'
-import { color } from '@arcadecity/ui'
-import { useArcadeRelay } from '@arcadecity/use-arcade/src/index'
+import { color, palette } from '@arcadecity/ui'
+import { Channel, useChannelsCreated, useArcadeRelay } from '@arcadecity/use-arcade/src/index'
 import Ionicons from '@expo/vector-icons/Ionicons'
+import { useEffect } from 'react'
 
 export function HomeScreen() {
   const sx = useSx()
 
   useArcadeRelay()
+  const channels: Channel[] = useChannelsCreated()
+
+  useEffect(() => {
+    console.log(channels)
+  }, [channels])
 
   return (
     <View
@@ -18,37 +24,10 @@ export function HomeScreen() {
         p: 16,
         backgroundColor: color.background,
       }}>
-      <H1 sx={{ color: color.text, fontWeight: '800' }}>Arcade Chat</H1>
-      <View sx={{ maxWidth: 600 }}>
-        <P sx={{ textAlign: 'center' }}>
-          follow us on {''}
-          <A
-            href='https://twitter.com/ArcadeCityHall'
-            // @ts-expect-error react-native-web only types
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-            sx={{ color: 'blue' }}>
-            twitter
-          </A>
-          .
-        </P>
-        <P sx={{ textAlign: 'center' }}>
-          Cross Platform Expo Vector Icon{' '}
-          <Ionicons name='md-checkmark-circle' size={32} color='green' />
-        </P>
-      </View>
-      <View sx={{ height: 32 }} />
-      <Row>
-        <TextLink
-          href='/user/satoshi'
-          textProps={{
-            style: sx({ fontSize: 16, fontWeight: 'bold', color: 'blue' }),
-          }}>
-          Regular Link
-        </TextLink>
-      </Row>
+      <H1 sx={{ color: color.text, fontWeight: '800' }}>Channels: {channels.length}</H1>
+      {channels.map((channel) => (
+        <P sx={{ color: palette.blueBell, textAlign: 'center' }}>{channel.name}</P>
+      ))}
     </View>
   )
 }
