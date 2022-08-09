76d46c3af0ea893e8d856525f4188150ab5a6661
Christopher David <chris@arcade.city>
2022-08-09 15:11:20 -0500

    Show pix

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,4 +1,4 @@
-import { useSx, View, H1, P, Row, A } from 'dripsy'
+import { useSx, View, H1, P, Row, A, Image } from 'dripsy'
 import { TextLink } from 'solito/link'
 import { color, palette } from '@arcadecity/ui'
 import { Channel, useChannelsCreated, useArcadeRelay } from '@arcadecity/use-arcade/src/index'
@@ -19,15 +19,24 @@ export function HomeScreen() {
     <View
       sx={{
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         p: 16,
         backgroundColor: color.background,
       }}>
-      <H1 sx={{ color: color.text, fontWeight: '800' }}>Channels: {channels.length}</H1>
-      {channels.map((channel) => (
-        <P sx={{ color: palette.blueBell, textAlign: 'center' }}>{channel.name}</P>
-      ))}
+      <View>
+        <H1 sx={{ color: color.text, fontWeight: '800' }}>Channels: {channels.length}</H1>
+        {channels.map((channel) => (
+          <Row style={{ marginVertical: 10, alignItems: 'center' }}>
+            <Image
+              source={{ uri: channel.picture }}
+              style={{ width: 70, height: 70, borderRadius: 35, marginRight: 20 }}
+            />
+            <P sx={{ color: palette.blueBell, textAlign: 'center' }}>
+              {channel.name} - {channel.about}
+            </P>
+          </Row>
+        ))}
+      </View>
     </View>
   )
 }
