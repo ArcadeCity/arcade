1c2209e3701ddd7d6d9e8832eaf3b5fc9e6ada81
Christopher David <chris@arcade.city>
2022-08-09 20:06:50 -0500

    Initial prism code block

--- packages/ui/src/library/ChannelPreviewScreen.tsx
+++ packages/ui/src/library/ChannelPreviewScreen.tsx
@@ -3,6 +3,14 @@ import { TextLink } from 'solito/link'
 import { Channel } from '../../../use-arcade/src'
 import { ChannelPreview } from '../molecules/ChannelPreview'
 import { color } from '../theme'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import clsx from 'clsx'
+
+const codeLanguage = 'typescript'
+const code = `<ChannelPreview
+  channel={channel}
+  onPress={() => console.log("Pressed channel", channel.id")}
+/>`
 
 export function ChannelPreviewScreen() {
   const dummyChannel: Channel = {
@@ -33,6 +41,24 @@ export function ChannelPreviewScreen() {
         channel={dummyChannel}
         onPress={() => console.log('Pressed ChannelPreview')}
       />
+
+      <Highlight {...defaultProps} code={code} language={codeLanguage}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={clsx(className, 'flex overflow-x-auto pb-6')}
+            style={{ ...style, padding: 12, borderRadius: 10 }}>
+            <code className='px-4'>
+              {tokens.map((line, lineIndex) => (
+                <div key={lineIndex} {...getLineProps({ line })}>
+                  {line.map((token, tokenIndex) => (
+                    <span key={tokenIndex} {...getTokenProps({ token })} />
+                  ))}
+                </div>
+              ))}
+            </code>
+          </pre>
+        )}
+      </Highlight>
     </View>
   )
 }
