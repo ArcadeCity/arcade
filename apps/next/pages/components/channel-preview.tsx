709435053f3458dafa799b1da0b9d00fe69d5ed5
Christopher David <chris@arcade.city>
2022-08-10 08:41:51 -0500

    Move prism piece to next not ui

--- apps/next/pages/components/channel-preview.tsx
+++ apps/next/pages/components/channel-preview.tsx
@@ -1,3 +1,36 @@
-import { ChannelPreviewScreen } from '@arcadecity/ui/src/library'
+import React from 'react'
+import { ChannelPreviewScreen } from '@arcadecity/ui/src'
 
-export default ChannelPreviewScreen
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import clsx from 'clsx'
+
+const codeLanguage = 'typescript'
+const code = `<ChannelPreview
+  channel={channel}
+  onPress={() => console.log("Pressed channel", channel.id")}
+/>`
+
+export default function () {
+  return (
+    <>
+      <ChannelPreviewScreen />
+      <Highlight {...defaultProps} code={code} language={codeLanguage}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={clsx(className, 'flex overflow-x-auto pb-6')}
+            style={{ ...style, padding: 12, borderRadius: 10 }}>
+            <code className='px-4'>
+              {tokens.map((line, lineIndex) => (
+                <div key={lineIndex} {...getLineProps({ line })}>
+                  {line.map((token, tokenIndex) => (
+                    <span key={tokenIndex} {...getTokenProps({ token })} />
+                  ))}
+                </div>
+              ))}
+            </code>
+          </pre>
+        )}
+      </Highlight>
+    </>
+  )
+}
--- packages/ui/src/library/ChannelPreviewScreen.tsx
+++ packages/ui/src/library/ChannelPreviewScreen.tsx
@@ -3,14 +3,6 @@ import { TextLink } from 'solito/link'
 import { Channel } from '../../../use-arcade/src'
 import { ChannelPreview } from '../molecules/ChannelPreview'
 import { color } from '../theme'
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import clsx from 'clsx'
-
-const codeLanguage = 'typescript'
-const code = `<ChannelPreview
-  channel={channel}
-  onPress={() => console.log("Pressed channel", channel.id")}
-/>`
 
 export function ChannelPreviewScreen() {
   const dummyChannel: Channel = {
@@ -31,7 +23,7 @@ export function ChannelPreviewScreen() {
       sx={{
         alignItems: 'center',
         backgroundColor: color.background,
-        flex: 1,
+        // flex: 1,
         paddingTop: 40,
       }}>
       <H1>ChannelPreview</H1>
@@ -43,24 +35,6 @@ export function ChannelPreviewScreen() {
       />
 
       <View style={{ height: 30 }} />
-
-      <Highlight {...defaultProps} code={code} language={codeLanguage}>
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre
-            className={clsx(className, 'flex overflow-x-auto pb-6')}
-            style={{ ...style, padding: 12, borderRadius: 10 }}>
-            <code className='px-4'>
-              {tokens.map((line, lineIndex) => (
-                <div key={lineIndex} {...getLineProps({ line })}>
-                  {line.map((token, tokenIndex) => (
-                    <span key={tokenIndex} {...getTokenProps({ token })} />
-                  ))}
-                </div>
-              ))}
-            </code>
-          </pre>
-        )}
-      </Highlight>
     </View>
   )
 }
