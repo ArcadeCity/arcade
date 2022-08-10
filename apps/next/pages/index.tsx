000e7e037e8fbb1f907f02c7347fb61a34bfc7a4
Christopher David <chris@arcade.city>
2022-08-10 16:35:08 -0500

    paths and type shaz

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { ChannelList } from '@arcadecity/ui'
+import { Channel, useArcadeRelay, useChannelsCreated } from '@arcadecity/use-arcade'
 
 export default function () {
+  useArcadeRelay()
+  const channels: Channel[] = useChannelsCreated()
   return (
     <div className='h-screen w-screen bg-haiti flex text-moonraker'>
       {/* Sidebar */}
       <div
-        className={`w-full md:!w-[350px] border-minsk h-screen flex-shrink-0 overflow-y-auto overflow-x-hidden border-r`}
-      >
-        <ChannelList />
+        className={`w-full md:!w-[350px] border-minsk h-screen flex-shrink-0 overflow-y-auto overflow-x-hidden border-r`}>
+        <ChannelList channels={channels} />
       </div>
 
       <div className='flex h-screen flex-grow flex-col items-stretch bg-purple'></div>
--- packages/use-arcade/src/index.ts
+++ packages/use-arcade/src/index.ts
@@ -1,23 +1,20 @@
 export { ArcadeContext } from './context'
 
-export { useArcadeRelay, UseArcadeRelayActions } from './hooks/useArcadeRelay'
+export { useArcadeRelay } from './hooks/useArcadeRelay'
+export type { UseArcadeRelayActions } from './hooks/useArcadeRelay'
 export { useBoolean } from './hooks/useBoolean'
 
 export * from './hooks'
 
-export {
-  AccountMetadata,
-  Channel,
-  ChannelMetadata,
-  Message,
-  setActiveChannelId,
-  store,
-  useRideRequests,
-} from './store'
+export { setActiveChannelId, store, useRideRequests } from './store'
+
+export type { AccountMetadata, Channel, ChannelMetadata, Message } from './store'
+
 export { updateChannelMetadata } from './store/updateChannelMetadata'
 
 export type { UseBoolean, UseBooleanActions } from './hooks/useBoolean'
 
-export { formatEvent, getKeysForMnemonic, hexToNpub, hexToNsec, NostrEvent, NostrKind } from './nostr'
+export { formatEvent, getKeysForMnemonic, hexToNpub, hexToNsec } from './nostr'
+export type { NostrEvent, NostrKind } from './nostr'
 
 export { updateDemoChannelMetadata } from './demo/updateDemoChannelMetadata'
