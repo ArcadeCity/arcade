eadec6aa2b7b6f0109c6f5274d76e960142d4df0
Christopher David <chris@arcade.city>
2022-08-10 16:07:27 -0500

    Chat UX stringup

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,3 +1,14 @@
-import { HomeScreen } from 'app/features/home/screen'
+import React from 'react'
 
-export default HomeScreen
+export default function () {
+  return (
+    <div className='h-screen w-screen bg-haiti flex text-moonraker'>
+      {/* Sidebar */}
+      <div
+        className={`w-full md:!w-[350px] border-minsk h-screen flex-shrink-0 overflow-y-auto overflow-x-hidden border-r`}
+      ></div>
+
+      <div className='flex h-screen flex-grow flex-col items-stretch bg-purple'></div>
+    </div>
+  )
+}
