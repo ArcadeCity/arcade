97105f914d785bc2a434fa07c6b740a4328b826b
Christopher David <chris@arcade.city>
2022-08-13 18:35:03 -0500

    usenostr hook pulled in yay

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react'
 import 'text-encoding-polyfill'
-// import { NativeNavigation } from 'app/navigation/native'
+import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
-import { Alert, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { useEffect } from 'react'
-// import { palette } from 'ui'
-import { palette } from 'ui/src/theme/palette'
-// import { helloMessage } from 'app/test'
 
 export default function App() {
-  useEffect(() => {
-    // Alert.alert(helloMessage)
-    Alert.alert('Testing!!! :)')
-  }, [])
   return (
     <Provider>
-      <View style={{ flex: 1, backgroundColor: palette.electricViolet }}>
-        <StatusBar style='light' />
-      </View>
+      <StatusBar style='light' />
+      <NativeNavigation />
     </Provider>
   )
 }
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,24 +1,16 @@
-import { useSx, View, H1, P } from 'dripsy'
-import { color, palette } from '@arcadecity/ui'
+import React from 'react'
+import { View, H1, P } from 'dripsy'
+import { color } from '@arcadecity/ui/src/theme/color'
 import {
-  Channel,
-  useChannelsCreated,
+  // Channel,
+  // useChannelsCreated,
   useNostr,
-  ArcadeContext,
-  UseArcadeRelayActions,
-} from '@arcadecity/use-arcade/src/index'
-import { useContext } from 'react'
-import { TouchableOpacity } from 'react-native'
-import { ChannelPreview } from '@arcadecity/ui'
+  // ArcadeContext,
+  // UseArcadeRelayActions,
+} from '@arcadecity/use-arcade/src/hooks/useNostr'
 
 export function HomeScreen() {
   useNostr()
-  const channels: Channel[] = useChannelsCreated()
-  const context = useContext(ArcadeContext) as any
-  const actions = context.actions as UseArcadeRelayActions
-  const createChannel = async () => {
-    actions.createChannel('TestChannel1', 'Aboutsogood', 'https://placekitten.com/200/300')
-  }
 
   return (
     <View
@@ -28,24 +20,7 @@ export function HomeScreen() {
         p: 16,
         backgroundColor: color.background,
       }}>
-      <View style={{ paddingTop: 40 }}>
-        <TouchableOpacity
-          activeOpacity={0.8}
-          onPress={createChannel}
-          style={{ backgroundColor: palette.electricIndigo, borderRadius: 10, marginVertical: 20 }}>
-          <P sx={{ color: palette.moonRaker, fontWeight: 'bold', textAlign: 'center' }}>
-            Create demo channel
-          </P>
-        </TouchableOpacity>
-        <H1 sx={{ color: color.text, fontWeight: '800' }}>Channels: {channels.length}</H1>
-        {channels.map((channel) => (
-          <ChannelPreview
-            key={channel.id}
-            channel={channel}
-            onPress={() => console.log(`Pressed channel ${channel.id}`)}
-          />
-        ))}
-      </View>
+      <View style={{ paddingTop: 40 }}></View>
     </View>
   )
 }
