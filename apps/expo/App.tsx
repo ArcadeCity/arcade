8fe06a062b6a4f1041d00e369a3a888779124118
Christopher David <chris@arcade.city>
2022-08-14 00:33:50 -0500

    Update on start didn't work - lets try useExpoUpdates

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -3,8 +3,10 @@ import 'text-encoding-polyfill'
 import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
 import { StatusBar } from 'expo-status-bar'
+import { useExpoUpdates } from './lib/useExpoUpdates'
 
 export default function App() {
+  useExpoUpdates(3)
   return (
     <Provider>
       <StatusBar style='light' />
