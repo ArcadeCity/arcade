c34f5435e86fb734d03319d1f9b5a852f92186a5
Christopher David <chris@arcade.city>
2022-08-15 16:34:33 -0500

    FadeInMap

--- packages/ui/src/molecules/map/FadeInMap.tsx
+++ packages/ui/src/molecules/map/FadeInMap.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
-import { View } from 'dripsy'
 import { Map } from './map'
+import { MotiView } from 'moti'
+import { palette } from 'ui/src/theme'
 
 export const FadeInMap = () => {
   return (
     <>
-      <View
+      <MotiView
+        delay={2500}
+        transition={{
+          type: 'timing',
+          duration: 2500,
+        }}
         style={{
           flex: 1,
-          backgroundColor: 'rgba(0,0,0,0.3)',
+          backgroundColor: palette.haiti,
           position: 'absolute',
           zIndex: 7888,
           top: 0,
           left: 0,
           right: 0,
           bottom: 0,
+        }}
+        from={{
+          opacity: 1,
+        }}
+        animate={{
+          opacity: 0.2,
         }}
       />
       <Map />
