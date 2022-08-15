533301c8b0f9853d7929657b900b2e3357a58d2b
Christopher David <chris@arcade.city>
2022-08-14 20:16:37 -0500

    Cover image blur

--- packages/app/features/home/Filter.tsx
+++ packages/app/features/home/Filter.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Dimensions } from 'react-native'
 import { Canvas, Image, Turbulence, DisplacementMap, useImage } from '@shopify/react-native-skia'
 
 export const Filter = () => {
-  const image = useImage(require('./oslo.jpg'))
+  const image = useImage(require('./splash.png'))
   if (!image) {
     return null
   }
+  const { width, height } = Dimensions.get('window')
   return (
-    <Canvas style={{ width: 256, height: 256 }}>
-      <Image image={image} x={0} y={0} width={256} height={256} fit='cover'>
+    <Canvas style={{ width, height }}>
+      <Image image={image} x={0} y={0} width={width} height={height} fit='cover'>
         <DisplacementMap channelX='g' channelY='a' scale={20}>
           <Turbulence freqX={0.01} freqY={0.05} octaves={2} seed={2} />
         </DisplacementMap>
