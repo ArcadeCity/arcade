8d61d63511ac369a09c054e371d4411a651614a8
Christopher David <chris@arcade.city>
2022-08-16 08:01:35 -0500

    Initial login validation

--- packages/app/features/login/LoginScreen.tsx
+++ packages/app/features/login/LoginScreen.tsx
@@ -1,16 +1,37 @@
 import React, { useState } from 'react'
 import { StyleSheet, TextInput, View } from 'react-native'
 import { color, palette, spacing } from 'ui/src'
+import { hexToNsec, useAccount } from '@arcadecity/use-arcade/src'
 
 export const LoginScreen = () => {
   const [loginAs, setLoginAs] = useState('')
+  const [account, accountActions] = useAccount() as any
+  if (!account || !accountActions) return <></>
+
+  const onChangeText = (text: string) => {
+    setLoginAs(text)
+    try {
+      if (loginAs.split(' ').length === 12) {
+        accountActions.login({ mnemonic: loginAs })
+      } else if (loginAs.startsWith('nsec')) {
+        accountActions.login({ nsec: loginAs })
+      } else if (isHex(loginAs)) {
+        accountActions.login({ nsec: hexToNsec(loginAs) })
+      } else {
+        console.log('Not valid access code')
+      }
+    } catch (e) {
+      console.log('Error trying to validate access code')
+    }
+  }
+
   return (
     <View style={styles.container}>
       <TextInput
         autoCapitalize='none'
         autoCorrect={false}
         autoFocus={true}
-        onChangeText={(text) => setLoginAs(text)}
+        onChangeText={onChangeText}
         placeholder='Enter access code'
         placeholderTextColor={palette.blueBell}
         style={styles.input}
@@ -34,3 +55,8 @@ const styles = StyleSheet.create({
     marginVertical: 30,
   },
 })
+
+function isHex(h) {
+  var a = parseInt(h, 16)
+  return a.toString(16) === h.toLowerCase()
+}
